505b25b3f3f9e0bb728d930094bc5ad5bcc5dd23
Hordur <hordurh15@ru.is>
2017-03-10 01:31:29 +0000

    added fading, incomplete

---
+++ UnityProject/Assets/FadeController.cs
@@ -0,0 +1,110 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FadeController : MonoBehaviour {
+
+    private SteamVR_TrackedController controller;
+    private GameObject eyes;
+
+    private bool fading = false;
+    private bool ready = true;
+
+    private int fadeOutDuration = 3;
+    private int fadeInDuration = 20;
+
+    public bool activeControllers = false;
+
+    void Start()
+    {
+        if (!activeControllers)
+        {
+            eyes = GameObject.Find("Camera (eye)");
+            if (eyes.GetComponent<SteamVR_Fade>() == null)
+            {
+                eyes.AddComponent<SteamVR_Fade>();
+            }
+        }
+    }
+
+    void Update()
+    {
+        if (!activeControllers)
+        {
+            if (Input.GetKeyDown(KeyCode.F) && !fading && ready)
+            {
+                ready = false;
+                print("read");
+                StartCoroutine(FadeToBlack());
+            }
+            else if (Input.GetKeyDown(KeyCode.F) && fading && ready)
+            {
+                ready = false;
+                StartCoroutine(FadeFromBlack());
+            }
+        }
+    }
+
+    void OnEnable()
+    {
+        if (activeControllers)
+        {
+            eyes = GameObject.Find("Camera (eye)");
+            if (eyes.GetComponent<SteamVR_Fade>() == null)
+            {
+                eyes.AddComponent<SteamVR_Fade>();
+            }
+            controller = GetComponent<SteamVR_TrackedController>();
+            controller.TriggerClicked += HandleTriggerClicked;
+            controller.PadClicked += HandlePadClicked;
+        }
+
+    }
+
+    void OnDisable()
+    {
+        if (activeControllers)
+        {
+            controller.TriggerClicked -= HandleTriggerClicked;
+            controller.PadClicked -= HandlePadClicked;
+        }
+
+    }
+
+    void HandleTriggerClicked(object sender, ClickedEventArgs e)
+    {
+        if (!fading && ready)
+        {
+            ready = false;
+            StartCoroutine(FadeToBlack());
+        }
+        else if (fading && ready)
+        {
+            ready = false;
+            StartCoroutine(FadeFromBlack());
+        }    
+    }
+
+    void HandlePadClicked(object sender, ClickedEventArgs e)
+    {
+        print("Pad Clicked");
+    }
+
+    IEnumerator FadeToBlack()
+    {
+        print("started");
+        SteamVR_Fade.Start(Color.black, fadeOutDuration);
+        yield return new WaitForSeconds(fadeOutDuration);
+        ready = true;
+        fading = true;
+        print("ended");
+    }
+
+    IEnumerator FadeFromBlack()
+    {
+        SteamVR_Fade.Start(Color.clear, fadeInDuration);
+        yield return new WaitForSeconds(fadeInDuration);
+        ready = true;
+        fading = false;
+    }
+}
---
+++ UnityProject/Assets/Follow.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Follow : MonoBehaviour {
+
+    public Transform target = null;
+
+
+
+
+	// Use this for initialization
+	void Start () {
+       
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = target.position;
+        transform.rotation = target.rotation;
+    }
+}
