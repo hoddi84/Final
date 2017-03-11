802dc7795b10241590b1a71667b2c86d18e4074d
hoddi84 <hordurh15@ru.is>
2017-03-11 13:19:46 +0000

    adding to eventmanager

--- UnityProject/Assets/_Scripts/EventManager.cs
+++ UnityProject/Assets/_Scripts/EventManager.cs
@@ -7,12 +7,49 @@ public class EventManager : MonoBehaviour {
     public delegate void ClickedEventHandler();
 
     public static event ClickedEventHandler MouseDownRight;
+    public static event ClickedEventHandler MouseDownPressedRight;
     public static event ClickedEventHandler MouseUpRight;
     public static event ClickedEventHandler MouseDownLeft;
+    public static event ClickedEventHandler MouseDownPressedLeft;
     public static event ClickedEventHandler MouseUpLeft;
 
     public static event ClickedEventHandler KeyE_Down;
     public static event ClickedEventHandler KeyE_Up;
-    public static event ClickedEventHandler KeyE_Pressed;
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (MouseDownLeft != null)
+            {
+                MouseDownLeft();
+            }
+        }
+        else if (Input.GetMouseButtonUp(0))
+        {
+            if (MouseUpLeft != null)
+            {
+                MouseUpLeft();
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            if (KeyE_Down != null)
+            {
+                KeyE_Down();
+            }
+        }
+        else if (Input.GetKeyUp(KeyCode.E)) 
+        {
+            if (KeyE_Up != null)
+            {
+                KeyE_Up();
+            }
+        }
+    }
 
 }
+
+
+
---
+++ UnityProject/Assets/_ScriptsTest/EventTester.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EventTester : MonoBehaviour {
+
+    void OnEnable()
+    {
+        EventManager.MouseDownLeft += MouseDownLeft;
+        EventManager.MouseUpLeft += MouseUpLeft;
+    }
+
+    void OnDisable()
+    {
+        EventManager.MouseDownLeft -= MouseDownLeft;
+        EventManager.MouseUpLeft -= MouseUpLeft;
+    }
+
+    void MouseDownLeft()
+    {
+        print("Left mouse button pressed down");
+    }
+
+    void MouseUpLeft()
+    {
+        print("Left mouse button released");
+    }
+}
