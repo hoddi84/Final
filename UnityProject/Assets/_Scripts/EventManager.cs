ec1b565d2d1d82ceafe08e68a0021bbf1cf0fb3f
hoddi84 <hordurh15@ru.is>
2017-03-11 00:46:45 +0000

    converting to events for main

---
+++ UnityProject/Assets/_Scripts/EventManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EventManager : MonoBehaviour {
+
+    public delegate void ClickedEventHandler();
+
+    public static event ClickedEventHandler MouseDownRight;
+    public static event ClickedEventHandler MouseUpRight;
+    public static event ClickedEventHandler MouseDownLeft;
+    public static event ClickedEventHandler MouseUpLeft;
+
+    public static event ClickedEventHandler KeyE_Down;
+    public static event ClickedEventHandler KeyE_Up;
+    public static event ClickedEventHandler KeyE_Pressed;
+
+}
