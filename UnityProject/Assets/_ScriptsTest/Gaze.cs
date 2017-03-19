b7db4aef883d454a38f61ac05e5d4cd1b4183122
Hordur <hordurh15@ru.is>
2017-03-19 17:28:13 +0000

    playing with gaze

---
+++ UnityProject/Assets/_ScriptsTest/Gaze.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gaze : MonoBehaviour {
+
+    public GameObject start;
+    public GameObject end;
+
+    private Vector3 dir;
+	// Use this for initialization
+	void Start () {
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        dir = start.transform.position - end.transform.position;
+        Debug.DrawLine(start.transform.position, end.transform.position, Color.green);
+	}
+
+    void FixedUpdate()
+    {
+        Vector3 fwd = transform.TransformDirection(Vector3.forward);
+        if (Physics.Raycast(start.transform.position, dir,100f))
+        {
+            print("hit");
+        }
+    }
+}
