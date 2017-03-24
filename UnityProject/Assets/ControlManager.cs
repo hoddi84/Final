2069da0c93fd46060aa0c1c5a583a210124544ae
hoddi84 <hordurh15@ru.is>
2017-03-24 17:12:38 +0000

    adding interaction events

---
+++ UnityProject/Assets/ControlManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlManager : MonoBehaviour {
+
+    public delegate void InteractEventHandler();
+
+    public static event InteractEventHandler MouseRightRaycastHit;
+    public static event InteractEventHandler MouseLeftRaycastHit;
+
+    public Camera overViewCam;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = overViewCam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit, 10))
+            {
+                if (MouseLeftRaycastHit != null)
+                {
+                    MouseLeftRaycastHit();
+                }
+            }
+        }
+    }
+}
