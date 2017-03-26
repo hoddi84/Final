ff5f7abd2b850d35222852da0ca8b14e31b82235
hoddi84 <hordurh15@ru.is>
2017-03-26 14:01:30 +0000

    added the InputHandler and InputMananger

---
+++ UnityProject/Assets/_ScriptsMain3/InputHandler.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputHandler : MonoBehaviour {
+
+    public delegate void RaycastEventHandler(GameObject obj);
+    public delegate void MouseEventHandler();
+
+    public static event RaycastEventHandler MouseRightRaycastHit;
+    public static event RaycastEventHandler MouseLeftRaycastHit;
+
+    public static event MouseEventHandler MouseDownRight;
+    public static event MouseEventHandler MouseUpRight;
+
+    public static event MouseEventHandler MouseDownLeft;
+    public static event MouseEventHandler MouseUpLeft;
+
+    public Camera overViewCam;
+	
+	// Update is called once per frame
+    void Update()
+    {
+        /*
+         * Handler for the mouse input. 
+         */
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = overViewCam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit, 10))
+            {
+                if (MouseLeftRaycastHit != null)
+                {
+                    MouseLeftRaycastHit(hit.transform.gameObject);
+                }
+            }
+            else
+            {
+                if (MouseDownLeft != null)
+                {
+                    MouseDownLeft();
+                }
+            }
+        }
+        if (Input.GetMouseButtonDown(1))
+        {
+            Ray ray = overViewCam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit, 10))
+            {
+                if (MouseRightRaycastHit != null)
+                {
+                    MouseRightRaycastHit(hit.transform.gameObject);
+                }
+            }
+            else
+            {
+                if (MouseDownRight != null)
+                {
+                    MouseDownRight();
+                }
+            }
+        }
+        if (Input.GetMouseButtonUp(0))
+        {
+            if (MouseUpLeft != null)
+            {
+                MouseUpLeft();
+            }
+        }
+        if (Input.GetMouseButtonUp(1))
+        {
+            if (MouseUpRight != null)
+            {
+                MouseUpRight();
+            }
+        }
+    }
+}
---
+++ UnityProject/Assets/_ScriptsMain3/InputManager.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputManager : MonoBehaviour {
+
+    public bool raycastHitRight;
+    public bool raycastHitLeft;
+    public bool mouseDownRight;
+    public bool mouseDownLeft;
+
+    void OnEnable()
+    {
+        InputHandler.MouseLeftRaycastHit += RaycastLeft;
+        InputHandler.MouseRightRaycastHit += RaycastRight;
+
+        InputHandler.MouseDownRight += MouseDownRight;
+        InputHandler.MouseDownLeft += MouseDownLeft;
+
+        InputHandler.MouseUpRight += MouseUpRight;
+        InputHandler.MouseUpLeft += MouseUpLeft;
+    }
+
+    void OnDisable()
+    {
+        InputHandler.MouseLeftRaycastHit -= RaycastLeft;
+        InputHandler.MouseRightRaycastHit -= RaycastRight;
+
+        InputHandler.MouseDownRight -= MouseDownRight;
+        InputHandler.MouseDownLeft -= MouseDownLeft;
+
+        InputHandler.MouseUpRight -= MouseUpRight;
+        InputHandler.MouseUpLeft -= MouseUpLeft;
+    }
+
+    void RaycastLeft(GameObject obj)
+    {
+        print("Hit Collider (left mouse), tag: " + obj.transform.tag);
+        raycastHitLeft = true;
+    }
+
+    void RaycastRight(GameObject obj)
+    {
+        print("Hit Collider (right mouse), tag: " + obj.transform.tag);
+        raycastHitRight = true;
+    }
+
+    void MouseDownRight()
+    {
+        print("Mouse Down (right)");
+        mouseDownRight = true;
+    }
+
+    void MouseDownLeft()
+    {
+        print("Mouse Down (left");
+        mouseDownLeft = true;
+    }
+
+    void MouseUpLeft()
+    {
+        print("Mouse Up (left)");
+        mouseDownLeft = false;
+        raycastHitLeft = false;
+    }
+
+    void MouseUpRight()
+    {
+        print("Mouse Up (right)");
+        mouseDownRight = false;
+        raycastHitRight = false;
+    }
+}
