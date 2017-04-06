8a874b26bfa19af3a6c29160c3b6c4fc4490cb4f
Hordur <hordurh15@ru.is>
2017-04-06 13:39:39 +0000

    testing arc pointer

---
+++ UnityProject/Assets/_ScriptsInProgress/ArcTeleport.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArcTeleport : MonoBehaviour 
+{
+    bool show = false;
+
+    private Valve.VR.InteractionSystem.ArcTest arc;
+    private SteamVR_TrackedController controller;
+
+    void OnEnable()
+    {
+        arc = GetComponent<Valve.VR.InteractionSystem.ArcTest>();
+        controller = GetComponent<SteamVR_TrackedController>();
+        controller.PadClicked += HandlePadClicked;
+    }
+
+    void OnDisable()
+    {
+        controller.PadUnclicked -= HandlePadUnclicked;
+    }
+
+    void HandlePadClicked(object sender, ClickedEventArgs e)
+    {
+        show = true;
+    }
+
+    void HandlePadUnclicked(object sender, ClickedEventArgs e)
+    {
+        show = false;
+    }
+	
+	// Update is called once per frame
+	void Update () 
+	{
+        if (show)
+        {
+            RaycastHit hitInfo;
+            arc.SetArcData(transform.forward, transform.forward*10, true, false);
+            if (arc.DrawArc(out hitInfo))
+            {
+
+            }
+        }
+	}
+}
