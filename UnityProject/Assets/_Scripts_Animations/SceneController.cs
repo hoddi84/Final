7771a6a6c798088e0299c5d7f6005822e7838d6a
hoddi84 <hordurh15@ru.is>
2017-04-11 00:40:36 +0000

    animation scene

---
+++ UnityProject/Assets/_Scripts_Animations/SceneController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneController : MonoBehaviour {
+
+    public GameObject headDown;
+    public GameObject headFollow;
+    public HeadLookController controller;
+
+    private bool followActivated = false;
+
+	// Use this for initialization
+	void Start () {
+
+        controller.target = headDown.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+        if (Input.GetKeyDown(KeyCode.E) && !followActivated)
+        {
+            followActivated = true;
+            print("active");
+        }
+        else if (Input.GetKeyDown(KeyCode.E) && followActivated)
+        {
+            followActivated = false;
+            print("deactive");
+        }
+
+        if (followActivated)
+        {
+            controller.target = headFollow.transform.position;
+        }
+        else
+        {
+            controller.target = headDown.transform.position;
+        }
+	}
+}
