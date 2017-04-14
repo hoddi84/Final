9c87c0373cdb6eab6a2fb6faa6ae413a6ff44ea2
Hordur <hordurh15@ru.is>
2017-04-14 14:54:53 +0000

    working on walking maze

---
+++ UnityProject/Assets/MazeController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MazeController : MonoBehaviour {
+
+    [Header("Transition Triggers")]
+    public GameObject TransitionFirst;
+
+    private BoxCollider transitionFirstCollider;
+
+	// Use this for initialization
+	void Start () {
+
+        transitionFirstCollider = TransitionFirst.GetComponent<BoxCollider>();
+
+        /*
+         * We want to disable the collider until the player has
+         * moved into the VR area so the collider won't be accidentally
+         * triggered.
+         */
+        transitionFirstCollider.enabled = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            transitionFirstCollider.enabled = true;
+            print("TransitionFirst enabled");
+        }
+	}
+}
---
+++ UnityProject/Assets/MazeTransitions.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MazeTransitions : MonoBehaviour {
+
+    [Header("Transitions")]
+    public GameObject transitionFrom;
+    public GameObject transitionBetween;
+    public GameObject transitionTo;
+
+	// Use this for initialization
+	void Start () {
+
+        /*
+         * Make sure the transition we can move towards is disabled.
+         */
+        transitionTo.SetActive(false);
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            /*
+             * When we enter the trigger we want to disable the transitionFrom
+             * gameObject and enable the transitionTo gameObject. 
+             */
+            transitionFrom.SetActive(false);
+            transitionTo.SetActive(true);
+        }
+    }
+}
