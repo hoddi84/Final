33c0a7089a902979c9372c0b0e6b7177f7b7f1ff
hoddi84 <hordurh15@ru.is>
2017-04-19 16:40:08 +0000

    working on maze, upto 10

--- UnityProject/Assets/_Scripts_Maze/MazeAnnouncer.cs
+++ UnityProject/Assets/_Scripts_Maze/MazeAnnouncer.cs
@@ -8,4 +8,10 @@ public class MazeAnnouncer {
     public static string Place2 = "Current Place: \n" + "We are in Place 2";
     public static string Place3 = "Current Place: \n" + "We are in Place 3";
     public static string Place4 = "Current Place: \n" + "We are in Place 4";
+    public static string Place5 = "Current Place: \n" + "We are in Place 5";
+    public static string Place6 = "Current Place: \n" + "We are in Place 6";
+    public static string Place7 = "Current Place: \n" + "We are in Place 7";
+    public static string Place8 = "Current Place: \n" + "We are in Place 8";
+    public static string Place9 = "Current Place: \n" + "We are in Place 9";
+    public static string Place10 = "Current Place: \n" + "We are in Place 10";
 }
--- UnityProject/Assets/_Scripts_Maze/MazeController.cs
+++ UnityProject/Assets/_Scripts_Maze/MazeController.cs
@@ -13,13 +13,19 @@ public class MazeController : MonoBehaviour {
     public bool place2Active;
     public bool place3Active;
     public bool place4Active;
+    public bool place5Active;
+    public bool place6Active;
+    public bool place7Active;
+    public bool place8Active;
+    public bool place9Active;
+    public bool place10Active;
 
     [Header("Announcers")]
     public Text stateText;
 
 	// Use this for initialization
 	void Start () {
-		
+
 	}
 	
 	// Update is called once per frame
@@ -38,6 +44,12 @@ public class MazeController : MonoBehaviour {
                 place2Active = false;
                 place3Active = false;
                 place4Active = false;
+                place5Active = false;
+                place6Active = false;
+                place7Active = false;
+                place8Active = false;
+                place9Active = false;
+                place10Active = false;
                 MazeUtility.AnnounceState(stateText, MazeAnnouncer.Place1);
             }
             else if (obj.tag == "Place2" && obj.activeInHierarchy)
@@ -46,6 +58,12 @@ public class MazeController : MonoBehaviour {
                 place2Active = true;
                 place3Active = false;
                 place4Active = false;
+                place5Active = false;
+                place6Active = false;
+                place7Active = false;
+                place8Active = false;
+                place9Active = false;
+                place10Active = false;
                 MazeUtility.AnnounceState(stateText, MazeAnnouncer.Place2);
             }
             else if (obj.tag == "Place3" && obj.activeInHierarchy)
@@ -54,6 +72,12 @@ public class MazeController : MonoBehaviour {
                 place2Active = false;
                 place3Active = true;
                 place4Active = false;
+                place5Active = false;
+                place6Active = false;
+                place7Active = false;
+                place8Active = false;
+                place9Active = false;
+                place10Active = false;
                 MazeUtility.AnnounceState(stateText, MazeAnnouncer.Place3);
             }
             else if (obj.tag == "Place4" && obj.activeInHierarchy)
@@ -62,7 +86,97 @@ public class MazeController : MonoBehaviour {
                 place2Active = false;
                 place3Active = false;
                 place4Active = true;
+                place5Active = false;
+                place6Active = false;
+                place7Active = false;
+                place8Active = false;
+                place9Active = false;
+                place10Active = false;
                 MazeUtility.AnnounceState(stateText, MazeAnnouncer.Place4);
+            }
+            else if (obj.tag == "Place5" && obj.activeInHierarchy)
+            {
+                place1Active = false;
+                place2Active = false;
+                place3Active = false;
+                place4Active = false;
+                place5Active = true;
+                place6Active = false;
+                place7Active = false;
+                place8Active = false;
+                place9Active = false;
+                place10Active = false;
+                MazeUtility.AnnounceState(stateText, MazeAnnouncer.Place5);
+            }
+            else if (obj.tag == "Place6" && obj.activeInHierarchy)
+            {
+                place1Active = false;
+                place2Active = false;
+                place3Active = false;
+                place4Active = false;
+                place5Active = false;
+                place6Active = true;
+                place7Active = false;
+                place8Active = false;
+                place9Active = false;
+                place10Active = false;
+                MazeUtility.AnnounceState(stateText, MazeAnnouncer.Place6);
+            }
+            else if (obj.tag == "Place7" && obj.activeInHierarchy)
+            {
+                place1Active = false;
+                place2Active = false;
+                place3Active = false;
+                place4Active = false;
+                place5Active = false;
+                place6Active = false;
+                place7Active = true;
+                place8Active = false;
+                place9Active = false;
+                place10Active = false;
+                MazeUtility.AnnounceState(stateText, MazeAnnouncer.Place7);
+            }
+            else if (obj.tag == "Place8" && obj.activeInHierarchy)
+            {
+                place1Active = false;
+                place2Active = false;
+                place3Active = false;
+                place4Active = false;
+                place5Active = false;
+                place6Active = false;
+                place7Active = false;
+                place8Active = true;
+                place9Active = false;
+                place10Active = false;
+                MazeUtility.AnnounceState(stateText, MazeAnnouncer.Place8);
+            }
+            else if (obj.tag == "Place9" && obj.activeInHierarchy)
+            {
+                place1Active = false;
+                place2Active = false;
+                place3Active = false;
+                place4Active = false;
+                place5Active = false;
+                place6Active = false;
+                place7Active = false;
+                place8Active = false;
+                place9Active = true;
+                place10Active = false;
+                MazeUtility.AnnounceState(stateText, MazeAnnouncer.Place9);
+            }
+            else if (obj.tag == "Place10" && obj.activeInHierarchy)
+            {
+                place1Active = false;
+                place2Active = false;
+                place3Active = false;
+                place4Active = false;
+                place5Active = false;
+                place6Active = false;
+                place7Active = false;
+                place8Active = false;
+                place9Active = false;
+                place10Active = true;
+                MazeUtility.AnnounceState(stateText, MazeAnnouncer.Place10);
             }
         }
     }
