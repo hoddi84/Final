c4c377ecd37d5a5ed8687189d5201735b98e8cea
hoddi84 <hordurh15@ru.is>
2017-08-30 22:42:31 +0000

    Adding to Procedure maze

---
+++ UnityProject/Assets/ProceduralMaze/Scripts/ProPathTrigger.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProPathTrigger : MonoBehaviour {
+
+    public GameObject player;
+    public GameObject[] enableForwardPaths;
+    public GameObject disablePreviousPath;
+
+    public int rndPathIndex;
+
+    private void Start()
+    {
+        foreach (GameObject path in enableForwardPaths)
+        {
+            path.SetActive(false);
+        }
+
+        rndPathIndex = Random.Range(0, enableForwardPaths.Length);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (player.GetInstanceID() == other.gameObject.GetInstanceID())
+        {
+            EnablePath(rndPathIndex);
+
+            DisablePath();
+        }
+    }
+
+    private void EnablePath(int pathIndex)
+    {
+        foreach (GameObject path in enableForwardPaths)
+        {
+            path.SetActive(false);
+        }
+        enableForwardPaths[pathIndex].SetActive(true);
+    }
+
+    private void DisablePath()
+    {
+        if (disablePreviousPath != null)
+        {
+            disablePreviousPath.SetActive(false);
+        }
+    }
+}
