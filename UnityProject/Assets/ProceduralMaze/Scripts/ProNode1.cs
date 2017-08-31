17e19b4a3c0bfa1a80b38d43cd3a19ffcefe65d6
unknown <hordurh15@TVD-8856.hir.is>
2017-08-31 12:00:55 +0000

    Procedure Maze

---
+++ UnityProject/Assets/ProceduralMaze/Scripts/ProNode1.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProNode1 : MonoBehaviour {
+
+    public GameObject player;
+    public GameObject content;
+
+    private ProNode2[] connectedNodes;
+
+    private void Start()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (player.GetInstanceID() == other.gameObject.GetInstanceID())
+        {
+            ActivateNextPath();
+        }
+    }
+
+    void ActivateNextPath()
+    {
+        connectedNodes = FindObjectsOfType<ProNode2>();
+
+        foreach (ProNode2 node in connectedNodes)
+        {
+            node.SetContentActive(false);
+        }
+
+        int rndPathIndex = Random.Range(0, connectedNodes.Length);
+
+        connectedNodes[rndPathIndex].SetContentActive(true);
+    }
+}
---
+++ UnityProject/Assets/ProceduralMaze/Scripts/ProNodePath.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProNodePath : MonoBehaviour {
+
+    private ProNode1 node;
+
+    private void Start()
+    {
+        node = FindObjectOfType<ProNode1>();
+        print(node.gameObject.name);
+    }
+}
