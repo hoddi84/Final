c917daee076d66bf35c381b312defcd68860e732
hoddi84 <hordurh15@ru.is>
2017-09-03 23:27:34 +0000

    Adding Maze Procedure

---
+++ UnityProject/Assets/ProceduralMaze/Scripts/ProMazeManager.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProMazeManager : MonoBehaviour {
+
+    public GameObject[] unitTypeA;
+    public GameObject[] unitTypeB;
+
+    private GameObject previousUnit = null;
+    private GameObject currentUnit = null;
+
+    private void Start()
+    {
+        int rndIndex = Random.Range(0, unitTypeA.Length);
+        currentUnit = Instantiate(unitTypeA[rndIndex].gameObject);
+        currentUnit.GetComponent<ProType>().onTriggerEntered = SpawnNextUnit;
+    }
+
+    void SpawnNextUnit(GameObject nextUnit)
+    {
+        ProType nextType = nextUnit.GetComponent<ProType>();
+
+        if (previousUnit != null)
+        {
+            Destroy(previousUnit);
+        }
+
+        previousUnit = currentUnit;
+        previousUnit.GetComponent<ProType>().onTriggerEntered = null;
+
+        int rndIndex;
+
+        switch (nextType.unitTypeConnecter)
+        {
+            case UnitType.TypeA:
+
+                rndIndex = Random.Range(0, unitTypeA.Length);
+                currentUnit = Instantiate(unitTypeA[rndIndex].gameObject);
+                break;
+
+            case UnitType.TypeB:
+
+                rndIndex = Random.Range(0, unitTypeB.Length);
+                currentUnit = Instantiate(unitTypeB[rndIndex].gameObject);
+                break;
+        }
+        UpdateActiveUnitSender(currentUnit);
+    }
+
+    void UpdateActiveUnitSender(GameObject newSender)
+    {
+        newSender.GetComponent<ProType>().onTriggerEntered = SpawnNextUnit;
+    }
+}
---
+++ UnityProject/Assets/ProceduralMaze/Scripts/ProType.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum UnitType
+{
+    TypeA,
+    TypeB,
+    TypeC,
+    TypeD,
+    TypeF
+}
+
+public class ProType : MonoBehaviour {
+
+    private const string PLAYER = "Player";
+
+    public Action<GameObject> onTriggerEntered = null;
+
+    public UnitType unitType;
+    public UnitType unitTypeConnecter;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == PLAYER)
+        {
+            if (onTriggerEntered != null)
+            {
+                onTriggerEntered(gameObject);
+            }
+        }
+    }
+
+}
